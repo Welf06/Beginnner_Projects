0f8eafc8a7dd21c176e5cb941d753646b9c7c92d
Ganesh Nathan <85446331+Welf06@users.noreply.github.com>
2021-12-27 18:05:49 +0530

    commit

--- number guessing game/functions.py
+++ number guessing game/functions.py
@@ -1,12 +1,14 @@
 from random import randint
 
 players = {}
-# to preserve the existing leaderboard 
+# to preserve the existing leaderboard
 with open("leaderboard.txt", 'r') as f:
-        for line in f:
-            players[line.split()[0]] =int(line.split()[1]) 
+    for line in f:
+        players[line.split()[0]] = int(line.split()[1])
+
+
 def game():
-    
+
     print('''
                             Welcome to Number Guessing Game
             
@@ -21,7 +23,7 @@ def game():
             break
         elif cmd == 'help':
             help()
-        elif cmd == 'lead': 
+        elif cmd == 'lead':
             leaderboard()
 
 
@@ -34,19 +36,20 @@ def help():
           quit      Exit the game
           lead      Displays leaderboard
           """)
-    
+
+
 def play():
     """(None) -> None \n
     starts the game
     """
-    
-    num = randint(1,100)
+
+    num = randint(1, 100)
     print(num)
-    count= 0
+    count = 0
     name = input("Enter your username: ").strip().lower()
     while True:
         try:
-            guess= int(input('Enter your guess: '))
+            guess = int(input('Enter your guess: '))
         except:
             print("Please enter a valid integer")
             continue
@@ -60,22 +63,27 @@ def play():
                 print("Lady luck smiles upon you! \nYou guessed it just 1 turn!")
                 players[name] = players.get(name, 0) + count
                 break
-            print("You guessed correctly! \nIt took you", count, "turns to guess correctly")
-            players[name] = players.get(name, 0) 
+            print("You guessed correctly! \nIt took you",
+                  count, "turns to guess correctly")
+            players[name] = players.get(name, 0)
             players[name] = count
-            
+
             break
-    with open('leaderboard.txt','w') as f:
-        for name, count in sorted(players.items(), key= lambda x : x[1]):
+    with open('leaderboard.txt', 'w') as f:
+        for name, count in sorted(players.items(), key=lambda x: x[1]):
             f.write("{} {}\n".format(name, count))
+
 
 def leaderboard():
     """(None) -> None
     Prints the leaderboard
     """
-    print("{name:<15}{score:<5}".format(name='NAME', score= 'SCORE'))
+    print("{name:<15}{score:<5}".format(name='NAME', score='SCORE'))
     with open("leaderboard.txt", 'r') as f:
         for line in f:
-            print("{name:<15}{score:<5}".format(name=line.split()[0], score= line.split()[1]))
-if __name__=='__main__':
+            print("{name:<15}{score:<5}".format(
+                name=line.split()[0], score=line.split()[1]))
+
+
+if __name__ == '__main__':
     game()
--- number guessing game/gui.py
+++ number guessing game/gui.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 from tkinter import font
+
+
 def start():
     # initialising the window
     window = tk.Tk()
-    
+
     # defining the specification of the windows
     window.geometry("800x600")
     window.config(bg='#165050')
@@ -11,18 +13,24 @@ def start():
     window.title('Number Guessing Game')
 
     # defining labels
-    title = tk.Label(window, text="Number Guessing Game", font=('Ariel', 50), fg = 'white', bg='#165050')
+    title = tk.Label(window, text="Number Guessing Game",
+                     font=('Ariel', 50), fg='white', bg='#165050')
     title.place(x=30, y=50)
     # defining buttons
-    exit_button = tk.Button(window, text="EXIT", font=('Arial', 30), fg='white', bg='Red', command=exit)
-    exit_button.place(x=610,y=480)
-    
-    leaderboard_button = tk.Button(window, text="LEADERBOARD", font=('Arial', 30), fg='white', bg='Blue' )
-    leaderboard_button.place(x=240,y=480)
-    
-    play_button = tk.Button(window, text="PLAY", font=('Arial', 30), fg='white', bg='Green')
-    play_button.place(x=70,y=480)
-    
+    exit_button = tk.Button(window, text="EXIT", font=(
+        'Arial', 30), fg='white', bg='Red', command=exit)
+    exit_button.place(x=610, y=480)
+
+    leaderboard_button = tk.Button(
+        window, text="LEADERBOARD", font=('Arial', 30), fg='white', bg='Blue')
+    leaderboard_button.place(x=240, y=480)
+
+    play_button = tk.Button(window, text="PLAY", font=(
+        'Arial', 30), fg='white', bg='Green')
+    play_button.place(x=70, y=480)
+
     window.mainloop()
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     start()
