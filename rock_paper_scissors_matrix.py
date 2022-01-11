13c0dc35b4bc8a375d48f903de5d6f561db59470
Ganesh Nathan <85446331+Welf06@users.noreply.github.com>
2022-01-11 12:14:09 +0530

    edit 1

--- rock_paper_scissors_matrix.py
+++ rock_paper_scissors_matrix.py
@@ -1,16 +1,16 @@
-from random import choice
+from random import randint
 # 0: draw 1:lost 2:won
 matrix = [[0,1,2],
           [2,0,1],
           [1,2,0]]
 choices = ["rock", "paper", "scissors"]
 choice_map = {"rock": 0, "paper":1, "scissors":2}
-comp_choice = choice(choices)
+comp_choice = randint(0,2)
 user_choice = input("Please choose rock, paper or scissors:\n").lower().strip()
-comp_choice = choice_map.get(comp_choice)
 user_choice = choice_map.get(user_choice)
 outcome = matrix[user_choice][comp_choice]
 outcomes = ["Tie", "You lost", "You won"]
+print(f"Compter's choice: {choices[comp_choice]}")
 print(outcomes[outcome])
 
 
